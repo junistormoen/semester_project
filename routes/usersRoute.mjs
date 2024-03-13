8f08bd8ba9d81fa85c657f204f99e471baf1e638
junistormoen <juni.stormoen@icloud.com>
2024-03-13 14:57:33 +0100

    Oppdatert alle funksjoner rundt bruker

--- routes/usersRoute.mjs
+++ routes/usersRoute.mjs
@@ -25,6 +25,34 @@ USER_API.get('/', (req, res, next) => {
     res.json(req.user); // Returnerer brukeren hvis den finnes.
 })*/
 
+USER_API.get("/profile", async (req, res) => {
+    try {
+        /*const token = req.header("Authorization");
+        console.log(token)
+        const decoded = jwt.verify(token, "secret_key");
+        console.log(decoded)*/
+        const userId = req.header("ID")
+        const user = await db.query(
+            "SELECT * FROM users WHERE userid = $1", 
+            [userId]
+        );
+
+        if(user.rows.length === 0){
+            return res.status(404).json({message: "User not found"});
+        };
+
+        res.json({
+            name: user.rows[0].name,
+            email: user.rows[0].email
+        });
+    } catch (error) {
+        console.log("FEIL")
+        console.error(error.message);
+        res.status(500).send("Server error");
+    };
+});
+
+
 USER_API.post("/login", async (req, res) => {
     const { email, password } = req.body;
 
@@ -39,17 +67,15 @@ USER_API.post("/login", async (req, res) => {
         };
 
         // Sammenlign passordet med hashen lagret i databasen
-        if(password !== user.rows[0].pswHash){
-            return res.status(401).json({ message: "Invalid email or password"})
+        if (password !== user.rows[0].pswHash) {
+            return res.status(401).json({ message: "Invalid email or password" })
         };
 
         // Generer JWT
-        const token = jwt.sign({ userId: user.rows[0].id }, "secret_key", {
-            expiresIn: "1h",
-        });
+        const token = jwt.sign({ userId: user.rows[0].id }, "secret_key");
 
         // Send JWT til klienten
-        res.json({ token });
+        res.json({ token , userId: user.rows[0].userid});
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Server error");
@@ -75,46 +101,47 @@ USER_API.post('/register', async (req, res) => {
 });
 
 
-/*USER_API.post('/', (req, res, next) => {
+USER_API.put('/profile', async (req, res) => {
+    try {
+        /*const token = req.header("Authorization");
+        console.log(token)
+        const decoded = jwt.verify(token, "secret_key");
+        console.log(decoded)*/
+        const userId = req.header("ID");
 
-    // This is using javascript object destructuring.
-    // Recomend reading up https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment#syntax
-    // https://www.freecodecamp.org/news/javascript-object-destructuring-spread-operator-rest-parameter/
-    const { name, email, password } = req.body;
+        const { name, email } = req.body;
+        
+        await db.query (
+            "UPDATE users SET name = $1, email = $2 WHERE userid = $3",
+            [name, email, userId]
+        );
 
-    if (name != "" && email != "" && password != "") {
-        const user = new User();
-        user.name = name;
-        user.email = email;
-
-        // TODO: Do not save passwords.
-        user.pswHash = password;
-
-        let exists = false;
-        // TODO: Check if user exists in database
-        if (user) {
-            exists = true;
-        };
-
-        if (!exists) {
-            users.push(user);
-            res.status(HTTPCodes.SuccesfullRespons.Ok).end();
-        } else {
-            res.status(HTTPCodes.ClientSideErrorRespons.BadRequest).end();
-        }
-
-    } else {
-        res.status(HTTPCodes.ClientSideErrorRespons.BadRequest).send("Mangler data felt").end();
+        res.json({ message: "User information updated successfully"});
+    } catch {
+        console.error(error.message);
+        res.status(500).send("Server error");
     }
-
-});*/
-
-USER_API.put('/:id', (req, res) => {
-    /// TODO: Edit user
 })
 
-USER_API.delete('/:id', (req, res) => {
-    /// TODO: Delete user.
+USER_API.delete('/profile', async (req, res) => {
+    try {
+        /*const token = req.header("Authorization");
+        console.log(token)
+        const decoded = jwt.verify(token, "secret_key");
+        console.log(decoded)*/
+        const userId = req.header("ID");
+        
+        const user = await db.query(
+            "DELETE FROM users WHERE userid = $1",
+            [userId]
+        );
+
+        res.json({ message: "User deleted successfully"});
+        
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server error")
+    };
 })
 
 export default USER_API
